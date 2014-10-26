d0e8f488a449242f99c6cd9eb49ffc83abd27904
ashabanov <me.antony.rain@gmail.com>
2014-10-26 23:54:35 +0300

    [ADDED]: added solution for issue #9.

--- src/issue #9/static/js/main.js
+++ src/issue #9/static/js/main.js
@@ -6,11 +6,31 @@ $(function() {
 			return '<i class="b-info">link</i>' + state.text;
 		}
 
-	$('.b-select').select2({
+	var selects = $('.b-select').select2({
 		formatResult: format,
 		formatSelection: format,
 		escapeMarkup: function(markup) {
 			return markup;
 		}
 	});
+
+  $.each(selects, function(index, element){
+    var select = $(element).data('select2');
+
+    select.onSelect = (function(original, linkClass) {
+      return function(data, event) {
+        var target;
+
+        if (event != null) {
+          target = $(event.target);
+        }
+
+        if (target != null && target.hasClass(linkClass)) {
+          alert(data.text);
+        } else {
+          return original.apply(this, arguments);
+        }
+      }
+    })(select.onSelect, 'b-info');
+  });
 });
